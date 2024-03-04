d56dd88a85ed0b478da51d223ca17c4736b886a3
MrXXX <2998480931@qq.com>
2024-03-04 13:52:16 +0800

    fix

--- pyerm/scripts/__init__.py
+++ pyerm/scripts/__init__.py
@@ -21,30 +21,5 @@
 # SOFTWARE.
 
 # Version: 0.1.5
-from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-setup(
-    name='pyerm',
-    version='0.1.5',
-    author='Yuxuan Shao',
-    author_email='yx_shao@qq.com',
-    description='This project is an experiment record manager for python based on SQLite DMS, which can help you efficiently save your experiment settings and results for later analysis.',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Mr-SGXXX/pyerm",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    packages=find_packages(),
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'export_xls=pyerm.scripts.export_xls:main',
-        ],
-    },
-)
+__all__ = []
