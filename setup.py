620127c05f963d8eb02c419d14aa1bc120471cae
MrXXX <2998480931@qq.com>
2024-03-06 11:35:06 +0800

    v0.1.7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Version: 0.1.6
+# Version: 0.1.7
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as f:
@@ -28,7 +28,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='pyerm',
-    version='0.1.6',
+    version='0.1.7',
     author='Yuxuan Shao',
     author_email='yx_shao@qq.com',
     description='This project is an experiment record manager for python based on SQLite DMS, which can help you efficiently save your experiment settings and results for later analysis.',
